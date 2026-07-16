b6453570c5066a05bdaf400850be418362c467a0
FrequeNCy144 <nc_origin@protonmail.com>
2026-07-16 16:52:00 +0200

    chore: bump version to 1.4
    Update versionCode to 13 and versionName to 1.4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.frequent.habits"
     minSdk = 24
     targetSdk = 36
-    versionCode = 12
-    versionName = "1.3"
+    versionCode = 13
+    versionName = "1.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
