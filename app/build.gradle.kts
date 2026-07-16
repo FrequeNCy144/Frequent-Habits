c260483abf820adf4f55be9aa76c8829c502e7b5
Frequency144 <nc_origin@protonmail.com>
2026-07-16 13:24:35 +0200

    Version bump auf 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.frequent.habits"
     minSdk = 24
     targetSdk = 36
-    versionCode = 10
-    versionName = "1.1"
+    versionCode = 12
+    versionName = "1.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
